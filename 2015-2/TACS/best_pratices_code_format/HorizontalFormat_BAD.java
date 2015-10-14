ae29c777145993af7fcb98693b80f019213a1749
Bruno Soares da Silva <brunosoares.inf@gmail.com>
2015-10-14 20:49:41 -0300

    - Adicionando código de exemplo para formatação horizontal;

---
+++ 2015-2/TACS/best_pratices_code_format/HorizontalFormat_BAD.java
@@ -0,0 +1,5 @@
+public class HorizontalFormat_BAD {
+	public static String getVersion() {
+		return new StringBuilder().append("Version : 6.0").append(System.getProperty("line.separator")).append("Name: Marshmallow").append(System.getProperty("line.separator")).toString();
+	}
+}
